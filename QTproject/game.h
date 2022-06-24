edd90f475959bea93217882318da2dcca1bbd611
NakanoMiku39 <91249276+NakanoMiku39@users.noreply.github.com>
2022-06-25 01:02:16 +0800

    Add files via upload
    新增卡牌，角色，城市类

--- QTproject/game.h
+++ QTproject/game.h
@@ -2,7 +2,7 @@
 #define GAME_H
 
 #include <QWidget>
-#include <QPixmap>
+#include <QString>
 
 namespace Ui {
 class game;
@@ -26,4 +26,32 @@ private:
     Ui::game *ui;
 };
 
+class cards//卡牌类
+{
+private:
+   QString card_name;//牌名
+public:
+    cards(QString name):card_name(name){}//构造函数
+
+};
+
+class character//角色类
+{
+private:
+    int hand;//手牌数
+public:
+    character(int h):hand(h){}//构造函数
+
+};
+
+class city//城市类
+{
+private:
+    QString city_name, color;//城市名,颜色
+    int existed_disease_cube;//存在疾病块数
+
+public:
+    city(QString name, QString c):city_name(name), color(c), existed_disease_cube(0){}//构造函数
+
+};
 #endif // GAME_H
